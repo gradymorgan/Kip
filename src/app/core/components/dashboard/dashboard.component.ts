103c52ad4c6e93c9fc0f1e6c5fc70a18609be518
Grady Morgan <grady@gradymorgan.com>
2025-04-14 09:26:42 -0500

    Revert all changes back to beta 39

--- src/app/core/components/dashboard/dashboard.component.ts
+++ src/app/core/components/dashboard/dashboard.component.ts
@@ -31,7 +31,6 @@ import { WidgetSimpleLinearComponent } from '../../../widgets/widget-simple-line
 import { WidgetTutorialComponent } from '../../../widgets/widget-tutorial/widget-tutorial.component';
 import { WidgetWindComponent } from '../../../widgets/widget-wind/widget-wind.component';
 import { WidgetLabelComponent } from '../../../widgets/widget-label/widget-label.component';
-import { WidgetVerticalDataChartComponent } from '../../../widgets/widget-vertical-data-chart/widget-vertical-data-chart.component';
 import { uiEventService } from '../../services/uiEvent.service';
 
 
@@ -81,8 +80,7 @@ export class DashboardComponent implements AfterViewInit, OnDestroy{
       WidgetTutorialComponent,
       WidgetWindComponent,
       WidgetPositionComponent,
-      WidgetLabelComponent,
-      WidgetVerticalDataChartComponent
+      WidgetLabelComponent
     ]);
 
     effect(() => {
--- src/app/core/services/widget.service.ts
+++ src/app/core/services/widget.service.ts
@@ -136,14 +136,6 @@ export class WidgetService {
       componentClassName: 'WidgetDataChartComponent'
     },
     {
-      name: 'Vertical Data Chart',
-      description: 'A vertical chart visualization that displays data in a top-down historical format, similar to those found on advanced marine instruments. The display shows the current value at the top with historical data flowing downward.',
-      icon: 'datachartWidget',
-      category: 'Components',
-      selector: 'widget-vertical-data-chart',
-      componentClassName: 'WidgetVerticalDataChartComponent'
-    },
-    {
       name: 'Autopilot Head',
       description: 'An Autopilot Head for supported Signal K autopilot devices.',
       icon: 'autopilotWidget',
